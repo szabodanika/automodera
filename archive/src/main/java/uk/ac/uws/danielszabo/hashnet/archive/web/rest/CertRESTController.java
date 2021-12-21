a374f602f368bf5ffce6bbe404e56e0b55572ed8
szabodanika <daniel.szabo99@outlook.com>
2021-12-21 18:56:14 +0000

    Added untracked files missing from 8b15ecda0653dee514a6e25fdefe65b4bd805429

---
+++ archive/src/main/java/uk/ac/uws/danielszabo/hashnet/archive/web/rest/CertRESTController.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) Daniel Szabo 2021.
+ *
+ * GitHub: https://github.com/szabodanika
+ * Email: daniel.szabo99@outlook.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.uws.danielszabo.hashnet.archive.web.rest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.ac.uws.danielszabo.common.model.network.cert.CertificateRequest;
+import uk.ac.uws.danielszabo.common.model.network.cert.NodeCertificate;
+import uk.ac.uws.danielszabo.common.model.network.message.Message;
+import uk.ac.uws.danielszabo.common.model.network.node.NodeStatus;
+import uk.ac.uws.danielszabo.hashnet.archive.service.ArchiveServiceFacade;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+@Slf4j
+@RestController
+@RequestMapping("cert")
+public class CertRESTController {
+
+  private final ArchiveServiceFacade archiveServiceFacade;
+
+  public CertRESTController(ArchiveServiceFacade archiveServiceFacade) {
+    this.archiveServiceFacade = archiveServiceFacade;
+  }
+
+  @PostMapping(value = "processedrequest", consumes = "application/XML")
+  public ResponseEntity postRequest(@RequestBody Message message) {
+    if (archiveServiceFacade.checkCertificate(message.getCertificate())) {
+      CertificateRequest certificateRequest = (CertificateRequest) message.getContent();
+      NodeCertificate certificate = certificateRequest.getNode().getCertificate();
+      log.info("Received processed certificate from " + message.getCertificate().getId());
+      archiveServiceFacade.saveCertificate(certificate);
+      return new ResponseEntity(HttpStatus.OK);
+    } else {
+      return new ResponseEntity(HttpStatus.FORBIDDEN);
+    }
+  }
+}
---
+++ common/src/main/java/uk/ac/uws/danielszabo/common/service/image/TopicServiceImpl.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) Daniel Szabo 2021.
+ *
+ * GitHub: https://github.com/szabodanika
+ * Email: daniel.szabo99@outlook.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.uws.danielszabo.common.service.image;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import uk.ac.uws.danielszabo.common.model.hash.Topic;
+import uk.ac.uws.danielszabo.common.repository.TopicRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class TopicServiceImpl implements TopicService {
+
+  private final TopicRepository topicRepository;
+
+  public TopicServiceImpl(TopicRepository topicRepository) {
+    this.topicRepository = topicRepository;
+  }
+
+  public Optional<Topic> findById(String id) {
+    return topicRepository.findById(id);
+  }
+
+  public List<Topic> findAll() {
+    return topicRepository.findAll();
+  }
+
+  @Override
+  public Topic save(Topic topic) {
+    return topicRepository.save(topic);
+  }
+
+}
