5b52470a36bcdf1ddf07b6c76e614dc87425bb55
Dániel Szabó <25702868+szabodanika@users.noreply.github.com>
2022-03-29 14:23:32 +0100

    - added build bash scripts

---
+++ bash-scripts/generate-keystores.sh
@@ -0,0 +1,54 @@
+#
+#  Copyright (c) Daniel Szabo 2022.
+#
+#  GitHub: https://github.com/szabodanika
+#  Email: daniel.szabo99@outlook.com
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+echo "1/4 Creating ./keystore folder if needed"
+
+mkdir ./keystore
+
+if [ ! -f ./keystore/automodera-archive.p12 ]
+then
+    echo "2/4 Creating PKCS12 key for Archive Node"
+
+    keytool -genkeypair -alias automodera-archive -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore ./keystore/automodera-archive.p12 -validity 3650
+else
+    echo "2/4 Archive Node PKCS12 key already exists."
+fi
+
+if [ ! -f ./keystore/automodera-integrator.p12 ]
+then
+    echo "3/4 Creating PKCS12 key for Integrator Node"
+
+    keytool -genkeypair -alias automodera-integrator -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore ./keystore/automodera-integrator.p12 -validity 3650
+else
+    echo "3/4 Integrator Node PKCS12 key already exists."
+fi
+
+if [ ! -f ./keystore/automodera-operator.p12 ]
+then
+    echo "4/4 Creating PKCS12 key for Operator Node"
+
+    keytool -genkeypair -alias automodera-operator -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore ./keystore/automodera-operator.p12 -validity 3650
+else
+    echo "4/4 Operator Node PKCS12 key already exists."
+fi
+
+
+echo "4/4 PKCS12 keys created."
---
+++ bash-scripts/run.sh
@@ -0,0 +1,62 @@
+#
+#
+#  Copyright (c) Daniel Szabo 2022.
+#
+#  GitHub: https://github.com/szabodanika
+#  Email: daniel.szabo99@outlook.com
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+
+
+
+
+clear
+
+cleanup() {
+    # kill all processes whose parent is this process
+    pkill -P $$
+}
+
+for sig in INT QUIT HUP TERM; do
+  trap "
+    cleanup
+    trap - $sig EXIT
+    kill -s $sig "'"$$"' "$sig"
+done
+trap cleanup EXIT
+
+source ./bash-scripts/generate-keystores.sh
+
+echo "1/4 Creating ./log and ./db folders if needed"
+mkdir ./log
+mkdir ./db
+
+echo "2/3 Starting 3 Automodera instances..."
+echo "Starting Operator"
+java                              -jar ./jars/operator-0.3.jar     --dbname="./db/operator.sqlite"    --server.port="9000" &> ./log/operator.log &
+echo "Starting Integrator"
+java -Djava.library.path="./lib"  -jar ./jars/integrator-0.3.jar   --dbname="./db/integrator.sqlite"  --server.port="9001" &> ./log/integrator.log &
+echo "Starting Archive"
+java -Djava.library.path="./lib"  -jar ./jars/archive-0.3.jar      --dbname="./db/archive.sqlite"     --server.port="9002" &> ./log/archive.log &
+
+echo "3/3 All nodes have been started. Press CTRL + C to shut them down. If the process exits on its own, all nodes terminated likely due to an error. See the the log folder for more info."
+
+echo "Operator Node: https://127.0.0.1:9000"
+echo "Integrator Node: https://127.0.0.1:9001"
+echo "Archive Node: https://127.0.0.1:9002"
+
+wait
